6a97dd7d9df6f3ee3c043cdd04b2935c2e4d6b5c
maskatiabid <maskatiabid@gmail.com>
2025-04-24 14:50:45 +0530

    Created ads folder and added right/left ad images

--- components/LeftAd.js
+++ components/LeftAd.js
@@ -1,11 +1,14 @@
-export default function LeftAd() {
+// components/LeftAd.js
+const LeftAd = () => {
   return (
-    <div className="fixed left-0 top-1/4 w-48 h-72 bg-gray-200">
+    <div className="fixed left-0 top-1/4 w-64 h-96">
       <img
-        src="/ads/left-ad.jpg" // Path to the placeholder image
+        src="/ads/left-ad.jpg"  // Corrected path
         alt="Left Ad"
         className="w-full h-full object-cover"
       />
     </div>
   );
-}
+};
+
+export default LeftAd;
--- components/RightAd.js
+++ components/RightAd.js
@@ -1,11 +1,14 @@
-export default function RightAd() {
+// components/RightAd.js
+const RightAd = () => {
   return (
-    <div className="fixed right-0 top-1/4 w-48 h-72 bg-gray-200">
+    <div className="fixed right-0 top-1/4 w-64 h-96">
       <img
-        src="/ads/right-ad.jpg" // Path to the placeholder image
+        src="/ads/right-ad.jpg"  // Corrected path
         alt="Right Ad"
         className="w-full h-full object-cover"
       />
     </div>
   );
-}
+};
+
+export default RightAd;
