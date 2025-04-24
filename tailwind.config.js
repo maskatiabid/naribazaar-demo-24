dde313a968d622b03b4776a3b03ed9b799d7a82c
maskatiabid <maskatiabid@gmail.com>
2025-04-24 13:53:37 +0530

    Updated tailwind.config.js to include app directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-// tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",  // Ensure it includes all files in the `pages` folder
-    "./components/**/*.{js,ts,jsx,tsx}",  // Include your components folder if applicable
-    // Add other directories where your HTML/JSX files exist
+    "./app/**/*.{js,ts,jsx,tsx}",  // Include the `app` folder
+    "./components/**/*.{js,ts,jsx,tsx}",  // Include the `components` folder
+    // Add other directories where your HTML/JSX files exist if needed
   ],
   theme: {
     extend: {},
